8e06d3e927caddfc0a13176fd9b1fc8bd9041536
Petr Krysl <pkrysl@ucsd.edu>
2023-04-17 19:07:51 -0700

    add maximum volume constraint

--- src/FinEtoolsTetsFromTris.jl
+++ src/FinEtoolsTetsFromTris.jl
@@ -2,18 +2,36 @@ module FinEtoolsTetsFromTris
 
 using FinEtools
 using TetGen
+using LinearAlgebra
 
-# tetunsuitable() do pa,pb,pc,pd
-#     vol=det(hcat(pb-pa,pc-pa,pd-pa))/6
-#     center=0.25*(pa+pb+pc+pd)-[0.5,0.5,0.5]
-#     vol> 0.05*norm(center)^2.5
-# end
+function _edgestatistics(fens, fes)
+    ncnodes = nodesperelem(fes)
+    esmin = Inf
+    esmax = 0.0
+    esmean = 0.0
+    nedges = 0
+    for i in 1:count(fes)
+        for f in 1:ncnodes
+            for s in f+1:ncnodes
+                es = norm(fens.xyz[fes.conn[i][s], :] - fens.xyz[fes.conn[i][f], :])
+                esmin = min(esmin, es)
+                esmax = max(esmax, es)
+                esmean += es
+                nedges += 1
+            end
+        end
+    end
+    return esmin, esmax, esmean / nedges
+end
 
-function mesh(fens, bfes)
+function mesh(fens, bfes; tetgen_args = "")
     input=TetGen.RawTetGenIO{Cdouble}()
     input.pointlist=fens.xyz'
     TetGen.facetlist!(input, connasarray(bfes)')
-    output =  tetrahedralize(input, "pQaq1.1")
+    esmin, esmax, esmean = _edgestatistics(fens, bfes)
+    maxvol = 1.5 * esmax^3 / 6
+    tetgen_args == "" && (tetgen_args = "pQq1.4a$(maxvol)")
+    output =  tetrahedralize(input, tetgen_args)
     nfens = deepcopy(fens)
     nfens.xyz = output.pointlist'
     fes = FESetT4(output.tetrahedronlist')
