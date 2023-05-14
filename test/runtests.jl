14da81f5df5f3d177efbd4e6f3c03e47da8eb22d
Petr Krysl <pkrysl@ucsd.edu>
2023-05-14 08:23:47 -0700

    add test the check the closed character

--- test/runtests.jl
+++ test/runtests.jl
@@ -125,3 +125,26 @@ end
 test()
 nothing
 end
+
+module m004
+using Test
+using FinEtools
+using FinEtools.MeshExportModule.VTKWrite
+using FinEtoolsTetsFromTris
+
+function test()
+    fens, fes = T4block(1.0, 2.0, 3.0, 11, 22, 33)
+    bfes = meshboundary(fes)
+    connected = findunconnnodes(fens, bfes);
+    fens, new_numbering = compactnodes(fens, connected);
+    bfes = renumberconn!(bfes, new_numbering);
+    # create an invalid surface set
+    bfes = subset(bfes, [1, 3])
+    @test_throws ErrorException fens, fes = FinEtoolsTetsFromTris.mesh(fens, bfes)
+
+    true
+end
+
+test()
+nothing
+end
